bdb54ba05fd47ea641044145876d9aa12b530881
VladislavRE250401 <99533628+VladislavRE250401@users.noreply.github.com>
2022-11-01 21:17:43 +0300

    Delete Task05/guess-number directory

--- Task05/guess-number/bin/guess-number.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$autoLoadGit = __DIR__.'/../vendor/autoload.php';
-$autoLoadPackgaist = __DIR__.'/../../../autoload.php';
-file_exists($autoLoadGit) ? require_once($autoLoadGit) : require_once($autoLoadPackgaist);
-use function vladislav_re250401\guess-number\Controller\startGame;
-startGame();
-
-?>
